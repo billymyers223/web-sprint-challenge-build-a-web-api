e742fb555ea8a067cf33cb469efe6973a7c8a104
billmyers223 <billymyers223@gmail.com>
2021-10-01 12:21:45 -0500

    Attempting to fix project post

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,29 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model')
+
+function validateProjId(req, res, next) {
+    const { id } = req.params;
+    Projects.get(id)
+      .then((possibleProject) => {
+        if (!possibleProject) {
+            next({ message: "Project with that ID not found", status: 404 });
+        } else {
+            req.user = possibleProject;
+            next();
+        }
+      })
+      .catch(next);
+  }
+
+  function validateProjBody(req, res, next) {
+    const {name, description} = req.body 
+    if(!name || !description) {
+      res.status(400).json({
+        message: "missing required name and description fields"
+      })
+    } else {
+
+      next()
+    }
+  }
+  module.exports ={validateProjId, validateProjBody}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,39 @@
 // Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+const router = express.Router();
+const { validateProjId, validateProjBody } = require('./projects-middleware')
+router.get('/', (req, res, next) => {
+    Projects.get()
+        .then(projs => {
+            res.status(200).json(projs);
+        })
+        .catch(error => {
+            next(error);
+        });
+});
+
+router.get("/:id", validateProjId, (req, res, next) => {
+    Projects.get(req.params.id)
+      .then((project) => {
+        res.status(200).json(project);
+      })
+      .catch(err =>{
+          next(err)
+        });
+});
+
+router.post('/', validateProjBody, (req, res, next) => {
+    Projects.insert(req.body)
+        .then(proj => {
+            res.status(201).json(proj);
+        })
+        .catch(err => {
+            next(err);
+        });
+});
+
+
+
+
+module.exports = router;
